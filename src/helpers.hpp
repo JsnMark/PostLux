e3b89a98c9c1ae192a63c4073053b09f521e9d57
JsnMark <jasen.y.mark@gmail.com>
2025-12-31 17:53:21 -0800

    shortest path implementation

---
+++ src/helpers.hpp
@@ -0,0 +1,95 @@
+//helpers.hpp
+
+#ifndef HELPERS_HPP
+#define HELPERS_HPP
+
+#include <vector>
+#include <exception>
+#include <iostream>
+#include <algorithm>
+
+
+
+
+/* 
+ * Takes a vector of costs and finds shortest path from top to bottom
+ * which minimizes total cost of the path. Path should not shift more than
+ * 1 spot between each row. 
+ *
+ * @param costs vector of costs
+ * @return path vector of length H. path[i] contains column index of path for each row i
+ */
+template <typename T>
+std::vector<size_t> shortestVerticalPath(std::vector<T> costs, size_t rows, size_t cols){
+    if(cols < 3){
+        throw std::runtime_error("Must have at least 3 columns");
+    }
+    if(costs.size() != rows * cols){
+        throw std::runtime_error("Invalid size of cost vector");
+    }
+
+    // Use new to avoid using resize() or vector copy constructor which are O(n)
+    // which could lead up to millions of iterations
+    T* accPtr = new T[costs.size()];
+    T* tmp = accPtr;
+    T* costData = costs.data();
+
+    // copy firstRow to accumulator
+    for(size_t i = 0; i < cols; ++i){
+        *tmp = *costData;
+        ++tmp;
+        ++costData;
+    }
+    T* choicesPtr = new T[costs.size()];
+
+    // DP. Take node with least cost
+    for(size_t i = 1; i < rows; ++i){
+        size_t min_idx;
+        T min_val;
+        for(size_t j = 0; j < cols; ++j){
+            min_idx = j;
+            min_val = accPtr[(i-1)*cols + j];
+            if(j >= 1 && accPtr[(i-1)*cols + j - 1] < min_val){
+                min_idx = j-1;
+                min_val = accPtr[(i-1)*cols + j - 1];
+            }
+            if(j + 1 < cols && accPtr[(i-1)*cols + j + 1] < min_val){
+                min_idx = j+1;
+                min_val = accPtr[(i-1)*cols + j + 1];
+            }
+
+            accPtr[i*cols + j] = costs[i*cols + j] + min_val;
+            choicesPtr[i*cols + j] = min_idx;
+        }
+    }
+
+    // Find the end of the path
+    size_t minIndex = costs.size() - 1;
+    T minVal = accPtr[minIndex];
+    size_t lastRow = rows - 1;
+    for(size_t j = 0; j < cols; ++j){
+        if(accPtr[lastRow * cols + j] < minVal){
+            minIndex = j;
+            minVal = accPtr[lastRow * cols + j];
+        }
+    }
+
+    // Add back the path, from back to front. Then reverse.
+    std::vector<size_t> path {};
+    path.emplace_back(minIndex);
+    for(size_t i = rows - 1; i > 0; --i){
+        minIndex = choicesPtr[i*cols + minIndex];
+        path.emplace_back(minIndex);
+    }
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+
+
+
+
+
+
+
+
+#endif
--- tests/playground.cpp
+++ tests/playground.cpp
@@ -7,31 +7,25 @@
 
 // #include "../src/image.hpp"
 #include "../src/core.hpp"
+#include "../src/helpers.hpp"
 
 using namespace cv;
 
 
-int main(int argc, char** argv){
-    Image<int> image {"../data/input/cind.jpg"};
-    std::cout << "Image has dimensions of: " << image.getRows() << ", " << image.getCols() << ", "<< image.getCha()<<std::endl;
-     Matrix<int> kernel {5,5,1};
-     kernel.fill(std::vector<int> {1,4,7,4,1,
-                                   4,16,26,16,4,
-                                   7,26,41,26,7,
-                                   4,16,26,16,4,
-                                   1,4,7,4,1,});
-     Image<int> imageCpy {image};
-     for(size_t i = 0; i < image.getRows(); ++i){
-         for(size_t j = 0; j < image.getCols(); ++j){
-             if (filterValidate(i, j, image, kernel)){
-                 for(size_t k = 0; k < image.getCha(); ++k){
-                     *(image.getPtr(i,j,k)) = applyFilter(i,j,k, imageCpy, kernel) / 273;
-                 }
-             }
-         }
-     }
-    image.saveImage("../data/output/imagef.jpg"); 
-    imageCpy.saveImage("../data/output/imageg.jpg");
+int main(){
+    
+    std::vector<int> costs = {1,0,1,1,
+                              1,1,0,1,
+                              1,1,1,0,
+                              1,1,1,0,
+                              1,1,0,1};
+    size_t rows = 5;
+    size_t cols = 4;
 
+    auto v = shortestVerticalPath(costs, rows, cols);
+    for(auto c : v){
+        std::cout << c << " ";
+    }
+    std::cout<<std::endl;
     return 0;    
 }
